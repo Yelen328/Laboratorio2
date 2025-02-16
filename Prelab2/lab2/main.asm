67de4373574d5763182716cd4110079ee9c0459f
Yelen328 <cot231076@uvg.edu.gt>
2025-02-16 01:37:34 -0600

    laboratorio
    parte del laboratorio agregada

--- Prelab2/lab2/main.asm
+++ Prelab2/lab2/main.asm
@@ -6,7 +6,7 @@
 ;
 ; Laboratorio_2.asm
 ;
-; Created: 14/2/2025 04:30:27
+; Created: 14/2/2025 09:49:27
 ; Author : yelen
 ;
 // Encabezado
@@ -15,7 +15,7 @@
 .org	SRAM_START
 .cseg
 .org	0x0000
-//.def	COUNTER=R20	//Contador para el Timer0
+.def	COUNTER=R20	//Contador para el Timer0
 
 //Stack
 LDI		R16, LOW(RAMEND)
@@ -29,13 +29,13 @@ TABLA:
 
 // Configuracion MCU
 SETUP:
-	/* Configurar Prescaler "Principal"
+	// Configurar Prescaler "Principal"
 	LDI R16, (1 << CLKPCE)
 	STS CLKPR, R16 // Habilitar cambio de PRESCALER
 	LDI R16, 0b00000100
 	STS CLKPR, R16 // Configurar Prescaler a 16 F_cpu = 1MHz
 	// Inicializar timer0
-	CALL INIT_TMR0*/
+	CALL INIT_TMR0
 
 	;PORTC COMO ENTRADA CON PULL-UP HABILIDATO 
 	LDI		R16, 0x00
@@ -58,7 +58,7 @@ SETUP:
 	// Deshabilitar serial (esto apaga los dem s LEDs del Arduino)
 	LDI R16, 0x00
 	STS UCSR0B, R16
-	//LDI COUNTER, 0x00
+	LDI COUNTER, 0x00
 
 	//variables iniciales 
 	LDI		R16, 0xFF
@@ -70,18 +70,30 @@ SETUP:
 /****************************************/
 // Loop Infinito
 MAIN_LOOP:
-	
+	IN R16, TIFR0 // Leer registro de interrupci n de TIMER 0?
+	SBRS R16, TOV0 // Salta si el bit 0 est "set" (TOV0 bit)?
+	RJMP MAIN_LOOP // Reiniciar loop
+	SBI TIFR0, TOV0 // Limpiar bandera de "overflow"
+	LDI R16, 100
+	OUT TCNT0, R16 // Volver a cargar valor inicial en TCNT0
+	INC COUNTER
+	CPI COUNTER, 10 // R20 = 10 after 100ms (since TCNT0 is set to 10 ms)
+	BRNE MAIN_LOOP
+	CLR COUNTER
+	CALL CONTADOR
+	S
+	RJMP MAIN_LOOP
 /****************************************/
 // NON-Interrupt subroutines
-/*INIT_TMR0:
+INIT_TMR0:
 	LDI R16, (1<<CS01) | (1<<CS00)
 	OUT TCCR0B, R16 // Setear prescaler del TIMER 0 a 64
 	LDI R16, 100
 	OUT TCNT0, R16 // Cargar valor inicial en TCNT0
-	RET*/
+	RET
 
-/*CONTADOR:
+CONTADOR:
 	INC R19
 	ANDI	R19,0x0F
 	OUT		PORTB, R19
-	RET*/
+	RET
